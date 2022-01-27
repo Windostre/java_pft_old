956066317ae134f5d234a480c4d8a100c95b3cca
Windostre <windostre@gmail.com>
2022-01-27 16:49:15 +0300

    Создан и модифицированы классы Square и Rectangle

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,28 +2,19 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
 
-    public static void main(String[] args) {
-        hello ("world");
-        hello ("user");
-        hello ("Mariya");
+  public static void main(String[] args) {
+    hello("world");
+    hello("user");
+    hello("Mariya");
 
-        double l = 5;
-        System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+    Square s = new Square(5); // Создание объекта класса Square, s это не сокращение. В скобках значение параметра из конструктора
+    System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area()); // s.area() обращение к методоу
 
-        double a = 4;
-        double b = 6;
-        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
-    }
+    Rectangle r = new Rectangle(4, 6); // Создание объекта класса Rectangle, r это не сокращение. В скобках значение параметра из конструктора
+    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+  }
 
-    public static void hello (String somebody) {
-        System.out.println("Hello, " + somebody +  "!");
-    }
-
-    public static double area (double len) {
-      return len * len;
-    }
-
-    public static double area(double a, double b) {
-        return a * b;
-    }
+  public static void hello(String somebody) {
+    System.out.println("Hello, " + somebody + "!");
+  }
 }
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Square.java
@@ -0,0 +1,15 @@
+package ru.stqa.pft.sandbox;
+
+public class Square {
+
+  public double l;
+
+  public Square(double l) { // Конструктор (псевдофункция), double l - параметры
+    this.l = l; //this - специальная переменная, которая ссылается на конструируемый объект. this.l = атрибут, l = переменная, которая объевлена внутри функции
+  }
+
+  public double area() {
+    return this.l * this.l;
+  }
+}
+
