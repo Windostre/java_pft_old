2ff9ac97954ffd22874d84adfe8069902e671ffd
Windostre <windostre@gmail.com>
2022-01-30 20:00:29 +0300

    изменена функция distance

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -4,23 +4,19 @@ public class MyFirstProgram {
 
   public static void main(String[] args) {
 
-    /* Задание №2 пункт  1*/
+    /* Задание 2.1  объявление переменных через созданный класс */
     Point p1 = new Point(2, 3);
     Point p2 = new Point(2, -1);
 
-    /* Задание №2 пункт  3*/
-    double result = distance(p1, p2); // вызов функции
-    System.out.println("Растояние между точками " + "p1 (" + p1.x + "," + p1.y + ")" + " и " + "p2 (" + p2.x + "," + p2.y + ")" + " = " + result);
+    /* Задание 2.3 запуск функции чере*/
+    distance(p1, p2); // вызов функции
+    System.out.println("Растояние между точками " + "p1 (" + p1.x + "," + p1.y + ")" + " и " + "p2 (" + p2.x + "," + p2.y + ")" + " = " + distance(p1, p2));
   }
 
-  /* Задание №2 пункт 2*/
+  /* Задание 2.2 функция */
   public static double distance(Point p1, Point p2) {
-    double deltax = p1.x - p2.x;
-    double deltay = p1.y - p2.y;
-    double sqrtdeltax = deltax * deltax;
-    double sqrtdeltay = deltay * deltay;
 
-    return Math.sqrt(sqrtdeltax + sqrtdeltay);
+    return Math.sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y));
 
   }
 
