9f345588d737f44196d0342df66c86c87260891f
Windostre <windostre@gmail.com>
2022-02-21 20:12:28 +0300

    Оптимизирован код в  GroupDeletionTests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupDeletionTests.java
@@ -1,54 +1,16 @@
 package ru.stqa.pft.addressbook;
 
-import java.time.Duration;
-
 import org.testng.annotations.*;
-import org.openqa.selenium.*;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class GroupDeletionTests {
-  private WebDriver wd;
+public class GroupDeletionTests extends TestBase {
 
-  @BeforeMethod(alwaysRun = true)
-  public void setUp() throws Exception {
-    wd = new FirefoxDriver();
-    wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(60));
-  }
 
   @Test
   public void testGroupDeletion() throws Exception {
-    wd.get("http://localhost/addressbook/");
-    wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys("admin");
-    wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys("secret");
-    wd.findElement(By.xpath("//input[@value='Login']")).click();
-    wd.findElement(By.linkText("groups")).click();
-    wd.findElement(By.name("selected[]")).click();
-    wd.findElement(By.xpath("//div[@id='content']/form/input[5]")).click();
-    wd.findElement(By.linkText("group page")).click();
+    gotoGroupPage();
+    selectGroup();
+    deleteSelectedGroups();
+   returnToGroupPage();
   }
 
-  @AfterMethod(alwaysRun = true)
-  public void tearDown() throws Exception {
-    wd.quit();
-  }
-
-  private boolean isElementPresent(By by) {
-    try {
-      wd.findElement(by);
-      return true;
-    } catch (NoSuchElementException e) {
-      return false;
-    }
-  }
-
-  private boolean isAlertPresent() {
-    try {
-      wd.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
-    }
-  }
 }
