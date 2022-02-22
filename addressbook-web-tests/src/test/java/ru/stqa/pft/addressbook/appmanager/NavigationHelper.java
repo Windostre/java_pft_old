e5a4b951814838adc48fae2ba9177125c657039a
Windostre <windostre@gmail.com>
2022-02-22 16:22:42 +0300

    В классе NavigationHelper наследован из HelperBase,  вместо внутренних методов использованы ссылки на методы click и type  из HelperBase

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -3,14 +3,13 @@ package ru.stqa.pft.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class NavigationHelper {
-  private WebDriver wd;  //Здесь код отличается от лекции: FirefoxDriver wd;
+public class NavigationHelper extends HelperBase {
 
   public NavigationHelper(WebDriver wd) {
-   this.wd = wd;
+   super(wd);
   }
 
   public void gotoGroupPage() {
-    wd.findElement(By.linkText("groups")).click();
+    click(By.linkText("groups"));
   }
 }
