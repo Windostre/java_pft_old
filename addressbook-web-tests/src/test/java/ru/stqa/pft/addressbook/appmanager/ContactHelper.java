91911b8d78e683936889b7396f47a18621a2f097
Windostre <windostre@gmail.com>
2022-02-24 16:31:54 +0300

    Добавлены новые методы в ContactHelper

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.appmanager;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import ru.stqa.pft.addressbook.model.ContactData;
@@ -22,5 +23,31 @@ public class ContactHelper extends HelperBase {
 
   public void initContactCreation() {
     click(By.linkText("add new"));
+  }
+  public void selectContact() {
+    click(By.name("selected[]"));
+  }
+  public void confirmContactDelete() {
+    click(By.linkText("home"));
+  }
+
+  public void pressDeleteContact() {
+    click(By.xpath("//input[@value='Delete']"));
+  }
+
+  public String closeAlertAndGetItsText() {
+    boolean acceptNextAlert = true;
+    try {
+      Alert alert = wd.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true; // Уточнить и поправить
+    }
   }
 }
