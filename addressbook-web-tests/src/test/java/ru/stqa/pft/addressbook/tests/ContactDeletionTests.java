ae2de51b48bdc914576fcc0bc8f91e44adb6eac2
Windostre <windostre@gmail.com>
2022-02-24 16:31:06 +0300

    Создан класс ContactDeletionTests

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -0,0 +1,24 @@
+package ru.stqa.pft.addressbook.tests;
+
+import java.util.concurrent.TimeUnit;
+import org.testng.annotations.*;
+import static org.testng.Assert.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import ru.stqa.pft.addressbook.tests.TestBase;
+
+public class ContactDeletionTests extends TestBase {
+  //private boolean acceptNextAlert = true;
+
+  @Test
+  public void testContactDeletion() throws Exception {
+    app.getContactHelper().selectContact();
+    //acceptNextAlert = true;
+    app.getContactHelper().pressDeleteContact();
+   assertTrue(app.getContactHelper().closeAlertAndGetItsText().matches("^Delete 1 addresses[\\s\\S]$")); // Уточнить и попправить
+    app.getContactHelper().confirmContactDelete();
+    app.getSessionHelper().logout();
+  }
+
+
+}
