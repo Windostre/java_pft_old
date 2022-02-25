793fde48765078910a328b55c84cf207615f1fd2
Windostre <windostre@gmail.com>
2022-02-25 16:10:57 +0300

    Заменены методы + добавлен новый в класс ContactDeletionTests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -1,26 +1,18 @@
 package ru.stqa.pft.addressbook.tests;
 
-import java.util.concurrent.TimeUnit;
 
 import org.testng.annotations.*;
 
-import static org.testng.Assert.*;
-
-import org.openqa.selenium.*;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import ru.stqa.pft.addressbook.tests.TestBase;
 
 public class ContactDeletionTests extends TestBase {
-  //private boolean acceptNextAlert = true;
 
   @Test
   public void testContactDeletion() throws Exception {
     app.getContactHelper().selectContact();
     app.getContactHelper().pressDeleteContact();
-    app.getContactHelper().closeAlert();
     app.getContactHelper().confirmContactDelete();
+    app.getNavigationHelper().goToHomePage();
     app.getSessionHelper().logout();
   }
-
 
 }
