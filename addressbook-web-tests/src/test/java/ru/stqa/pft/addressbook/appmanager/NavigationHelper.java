67896d1fd9df3a2399c6a0839321fa7eae54ae36
Windostre <windostre@gmail.com>
2022-03-02 20:18:15 +0300

    NavigationHelper оптимизированы переходы между страницами

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -6,20 +6,30 @@ import org.openqa.selenium.WebDriver;
 public class NavigationHelper extends HelperBase {
 
   public NavigationHelper(WebDriver wd) {
-   super(wd);
+    super(wd);
   }
 
   public void gotoGroupPage() {
+    if (isElementPresent(By.tagName("h1"))
+            && wd.findElement(By.tagName("h1")).getText().equals("Groups")
+            && isElementPresent(By.name("new"))) {
+      return;
+    }
     click(By.linkText("groups"));
   }
+
   public void returnToGroupPage() {
     click(By.linkText("group page"));
   }
+
   public void returnLinkToHomePage() {
     click(By.linkText("home page"));
   }
 
   public void goToHomePage() {
+    if (isElementPresent(By.id("maintable"))) {
+      return;
+    }
     click(By.partialLinkText("home"));
   }
 }
