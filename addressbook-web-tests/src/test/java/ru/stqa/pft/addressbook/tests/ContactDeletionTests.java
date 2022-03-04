b10a1b2f99a031004a7334b305c1e7e280ce1fbb
Windostre <windostre@gmail.com>
2022-03-04 16:14:41 +0300

    В тестах на удаление и создание контакта добавлены предусловия на создание контакта и проверка на наличие группы

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -3,17 +3,26 @@ package ru.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.GroupData;
 
 
 public class ContactDeletionTests extends TestBase {
 
   @Test
   public void testContactDeletion() throws Exception {
+    /* Предусловие: проверка наличия контакта, если нет - создать*/
     if (!app.getContactHelper().isThereAContact()) {
+      app.getNavigationHelper().gotoGroupPage();
+      /* Проверка наличия группы для создания контакта, если нет - создать*/
+      if (!app.getGroupHelper().isThereAGroup()) {
+        app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+      }
       app.getContactHelper().createContact(new ContactData("Mariya", "Ivanovna", "Petrova"
               , "TestNick", "3 Vavilova STR", "+7(123)123-12-12", "test_mail@test.com"
               , null), true);
     }
+
+    /* Тест */
     app.getNavigationHelper().goToHomePage();
     app.getContactHelper().selectContact();
     app.getContactHelper().pressDeleteContact();
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -2,16 +2,25 @@ package ru.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.GroupData;
 
 public class ContactModificationTests extends TestBase {
 
   @Test
   public void testContactModification() {
+    /* Предусловие: проверка наличия контакта, если нет - создать*/
     if (!app.getContactHelper().isThereAContact()) {
+      app.getNavigationHelper().gotoGroupPage();
+      /* Проверка наличия группы для создания контакта, если нет - создать*/
+      if (!app.getGroupHelper().isThereAGroup()) {
+        app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+      }
       app.getContactHelper().createContact(new ContactData("Mariya", "Ivanovna", "Petrova"
               , "TestNick", "3 Vavilova STR", "+7(123)123-12-12", "test_mail@test.com"
               , null), true);
     }
+
+    /* Тест */
     app.getNavigationHelper().goToHomePage();
     app.getContactHelper().selectContact();
     app.getContactHelper().initContactModification();
