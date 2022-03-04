154d9fdde488c6a3704d0aef865118f15805d580
Windostre <windostre@gmail.com>
2022-03-04 16:09:26 +0300

    В testContactCreation добавлено предусловие на создании группы (если отсутствует)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -8,10 +8,17 @@ public class ContactCreationTests extends TestBase {
 
   @Test
   public void testContactCreation() throws Exception {
+    /*  Проверка наличия группы для создания контакта, если нет - создать */
+    app.getNavigationHelper().gotoGroupPage();
+    if (!app.getGroupHelper().isThereAGroup()) {
+      app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+    }
+
+    /* Тест */
     app.getContactHelper().initContactCreation();
-    app.getContactHelper().fillContactForm(new ContactData("Mariya","Ivanovna", "Petrova"
-    , "TestNick", "3 Vavilova STR", "+7(123)123-12-12","test_mail@test.com"
-    , null), true);
+    app.getContactHelper().fillContactForm(new ContactData("Mariya", "Ivanovna", "Petrova"
+            , "TestNick", "3 Vavilova STR", "+7(123)123-12-12", "test_mail@test.com"
+            , null), true);
     app.getContactHelper().submitContactCreation();
     app.getNavigationHelper().returnLinkToHomePage();
     app.getSessionHelper().logout();
