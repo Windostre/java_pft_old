0f3ab2231bd183ae26316cac8c6b733d81c325e1
Windostre <windostre@gmail.com>
2022-03-15 17:54:32 +0300

    В тест по модификации группы добавлена проверка на списки

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupModificationTests extends TestBase {
@@ -15,15 +16,20 @@ public class GroupModificationTests extends TestBase {
       app.getGroupHelper().createGroup(new GroupData("group1", null, null));
     }
     List<GroupData> before = app.getGroupHelper().getGroupList();
-    //int before = app.getGroupHelper().getGroupCount();
     app.getGroupHelper().selectGroup(before.size() - 1);
     app.getGroupHelper().initGroupModification();
-    app.getGroupHelper().fillGroupForm(new GroupData("modifiedGroup1", "modifiedHeader", "ModifiedFooter"));
+    GroupData group = new GroupData(before.get(before.size() - 1).getId(),"modifiedGroup1", "modifiedHeader", "ModifiedFooter");
+    app.getGroupHelper().fillGroupForm(group);
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returnToGroupPage();
+
     List<GroupData> after = app.getGroupHelper().getGroupList();
-    //int after = app.getGroupHelper().getGroupCount();
     Assert.assertEquals(after.size(), before.size());
+
+    before.remove(before.size() - 1);
+    before.add(group);
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+
     app.getSessionHelper().logout();
 
   }
