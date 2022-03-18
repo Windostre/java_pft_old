9feed31b021e3c3c41b2ecec8af4add05ab331ef
Windostre <windostre@gmail.com>
2022-03-18 15:12:27 +0300

    В классе GroupCreationTests цикла поиска и сравнения максимального выражения заменен на ламбда выражение

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -3,7 +3,6 @@ package ru.stqa.pft.addressbook.tests;
 import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.GroupData;
-
 import java.util.HashSet;
 import java.util.List;
 
@@ -19,13 +18,17 @@ public class GroupCreationTests extends TestBase {
     Assert.assertEquals(after.size(), before.size() + 1);
     app.getSessionHelper().logout();
 
+    /* Способ сравнения при помощи цикла
     int max = 0;
     for (GroupData g : after) {
       if (g.getId() > max) {
         max = g.getId();
       }
     }
-    group.setId(max);
+    */
+
+    /* Новый способ сравнения при помощи ламбда выражения*/
+    group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(group);
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
